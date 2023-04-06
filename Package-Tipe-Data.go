fafd315cf56d2bce367c46affb9eae92d1cafd88
Hafiz Nur <hafiznur@live.com>
2023-04-06 15:51:16 +0700

    update latihan contoh tipe data v1.0.1

--- Package-Tipe-Data.go
+++ Package-Tipe-Data.go
@@ -1,5 +1,10 @@
 package Packagetipedata
 
-func Tipedata() string{
-	return "halo"
+func Tipedata() (int, string, float64, bool) {
+	contohinteger := 12
+	contohstring := "apa saja boleh"
+	contohfloat64 := 1.22668
+	ontohbool := false
+
+	return contohinteger, contohstring, contohfloat64, contohbool
 }
