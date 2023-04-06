38379fc5c284a1f4163afef65b95b7edd2531065
Hafiz Nur <hafiznur@live.com>
2023-04-06 16:27:01 +0700

    update ke sekian v1.0.2

--- Package-Tipe-Data.go
+++ Package-Tipe-Data.go
@@ -3,10 +3,11 @@ package Packagetipedata
 import "fmt"
 
 func Tipedata() {
-	Contohinteger := 12
-	Contohstring := "apa saja boleh"
-	Contohfloat64 := 1.22668
-	var Contohbool bool = false
+	// Contohinteger := 12
+	// Contohstring := "apa saja boleh"
+	// Contohfloat64 := 1.22668
+	// var Contohbool bool = false
 
-	fmt.Println(Contohinteger, Contohstring, Contohfloat64, Contohbool)
+	// fmt.Println(Contohinteger, Contohstring, Contohfloat64, Contohbool)
+	fmt.Println("halo worlds")
 }
